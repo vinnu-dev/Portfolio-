69de7c66c8ad07ea3b040f8e8158136360af75d3
vinnu-dev <katakamvinaychary@gmail.com>
2025-06-18 11:37:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function sayhello(){
+  alert ("this is from js file using anchor tag ")
+}
+function alert1(){
+  alert("opened the alert successfully");
+}
+function darkmode() {
+  document.body.classList.toggle("darkMode");
+}
+function togglemenu(){
+  var menu =
+  document.getElementById("menupopupp");
+  menu.style.display=
+  (menu.style.display==="block")?
+  "none" : "block";
+  
+}
